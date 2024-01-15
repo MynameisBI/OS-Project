7292e4b17bc451b0261610fda9074ee4e5a2f306
Rumis <20.21.9.12.1bibi@gmail.com>
2024-01-15 17:29:34 +0700

    Fix OS-independent filesystem bug

--- server/src/game/game.lua
+++ server/src/game/game.lua
@@ -24,12 +24,12 @@ function Game:enter(from, gameTheme)
 
   local wordArray = {}
   local totalWords = 0
-  local wordFile
-  if gameTheme == 'dog' then wordFile = io.open("assets/DogBreeds.txt", "r")
-  elseif gameTheme == 'cat' then wordFile = io.open("assets/CatBreeds.txt", "r")
-  elseif gameTheme == 'skeleton' then wordFile = io.open("assets/Bones.txt", "r")
+  local filename
+  if gameTheme == 'dog' then filename = "assets/DogBreeds.txt"
+  elseif gameTheme == 'cat' then  filename = "assets/CatBreeds.txt", "r"
+  elseif gameTheme == 'skeleton' then  filename = "assets/Bones.txt", "r"
   end
-  for line in wordFile:lines() do 
+  for line in love.filesystem.lines(filename) do 
     table.insert(wordArray, line)
     totalWords = totalWords + 1
   end
